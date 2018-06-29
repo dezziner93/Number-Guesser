b1ebb2d70e26600f12a2b9413b6c0cff6e450b23
dezziner93 <40169793+dezziner93@users.noreply.github.com>
2018-06-30 02:10:11 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,96 @@
+
+// GAME FUNCTION:
+// - Player must guess a number between a min and max
+// - Player gets a certain amount of guesses
+// - Notify player of guesses remaining
+// - Notify the player of the correct answer if loose
+// - Let player choose to play again
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Element 
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Asign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;    
+
+// Play again event listener
+game.addEventListener('click', function(e){
+  console.log(1);
+  // if(e.target.className === 'play-again'){
+  //   window.location.reload();
+  // }
+});
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+  
+  // Validate
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if(guess === winningNum){
+
+    // Game over - won
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+  } else {
+    // Wrong number
+    guessesLeft -= 1;
+
+    if(guessesLeft === 0){
+
+      // Game over - lost
+      gameOver(false, `Game Over, You lost. The correct number was ${winningNum}`);
+
+    } else {
+      // Game Continues - answer wrong
+      
+      // Change border color
+      guessInput.style.borderColor = 'red';
+
+      // Clear Input
+      guessInput.value = '';
+
+      // Tell user its the wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses Left`, 'red')
+    }
+  }
+
+});
+
+// Game over
+function gameOver(won, msg){
+  won === true ? color = 'green' : color = 'red';
+
+  // Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
+
+  // Play Again?
+  guessBtn.value = 'play Again';
+  guessBtn.className += 'play-again';
+}
+
+// Set Message
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
